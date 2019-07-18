ede9d324958e4386b27777615b1ddf19dedb3f06
Maurits van Altvorst <mvanaltvorst@icloud.com>
2019-07-18 22:31:22 +0200

    Improve error handling

--- index.js
+++ index.js
@@ -3,25 +3,31 @@
 const puppeteer = require('puppeteer');
 const fs = require("fs");
 
-
-const username = fs.readFileSync("/run/secrets/hackerrank-username").trim();
-const password = fs.readFileSync("/run/secrets/hackerrank-password").trim();
+const username = fs.readFileSync("/run/secrets/hackerrank-username", "utf-8").trim();
+const password = fs.readFileSync("/run/secrets/hackerrank-password", "utf-8").trim();
 
 (async () => {
-    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
-    const page = await browser.newPage();
-    await page.goto('https://www.hackerrank.com/auth/login?h_l=body_middle_left_button&h_r=login');
-    await page.type('#input-1', username);
-    await page.type('#input-2', password);
-    await page.click('button[type="submit"]');
-    await page.waitForNavigation();
+    let browser;
+    try {
+        browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+        const page = await browser.newPage();
+        await page.goto('https://www.hackerrank.com/auth/login?h_l=body_middle_left_button&h_r=login');
+        await page.type('#input-1', username);
+        await page.type('#input-2', password);
+        await page.click('button[type="submit"]');
+        await page.waitForNavigation();
 
-    if (page.url() != 'https://www.hackerrank.com/dashboard') {
-        console.log('Login failed! Are the username and password correct?');
+        if (page.url() != 'https://www.hackerrank.com/dashboard') {
+            console.log('Login failed! Are the username and password correct?');
+            await browser.close();
+            process.exit(1);
+        } else {
+            console.log('Login succeeded!');
+            await browser.close();
+        }
+    } catch (e) {
+        console.log("Caught exception: ", e);
         await browser.close();
         process.exit(1);
-    } else {
-        console.log('Login succeeded!');
-        await browser.close();
     }
 })()
